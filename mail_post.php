dcdca9af899b42b4db41e1031aba798d450bb3b8
Kolesnev Vova <vakolesnev@yandex.ru>
2019-06-24 14:23:52 +0700

    fix mail_post.php

--- mail_post.php
+++ mail_post.php
@@ -16,8 +16,6 @@ if (isset($_POST)) {
         $messages = trim($_POST["your-message"]);
         $url      = trim($_POST["url"]);
 
-
-
         $client  = @$_SERVER['HTTP_CLIENT_IP'];
         $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
         $remote  = @$_SERVER['REMOTE_ADDR'];
@@ -79,6 +77,35 @@ if (isset($_POST)) {
         $headers .= "From: Интернет-агенство msk.lapkinlab.ru <mail@lapkinlab.ru>\r\n";
         $headers .= 'Cc: akapinos@lapkinlab.ru' . "\r\n";
         $headers .= "Bcc: akapinos@lapkinlab.ru\r\n";
+        ?>
+        <!-- Facebook Pixel Code -->
+        <script>
+            !function(f, b, e, v, n, t, s) {
+                if (f.fbq) return;
+                n = f.fbq = function() {
+                    n.callMethod ?
+                        n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push    = n;
+                n.loaded  = !0;
+                n.version = '2.0';
+                n.queue   = [];
+                t         = b.createElement(e);
+                t.async   = !0;
+                t.src     = v;
+                s         = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+            }(window, document, 'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '754412144956224');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                       src="https://www.facebook.com/tr?id=754412144956224&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Facebook Pixel Code -->
+        <?
         mail($to, $subject, $message, $headers);
 
         require 'vendor/autoload.php';
@@ -142,7 +169,6 @@ if (isset($_POST)) {
             $messages = trim($_POST["your-message"]);
             $url      = trim($_POST["url"]);
 
-
             $client  = @$_SERVER['HTTP_CLIENT_IP'];
             $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
             $remote  = @$_SERVER['REMOTE_ADDR'];
@@ -183,6 +209,35 @@ if (isset($_POST)) {
             $headers .= "From: Интернет-агенство msk.lapkinlab.ru <mail@lapkinlab.ru>\r\n";
             $headers .= 'Cc: akapinos@lapkinlab.ru' . "\r\n";
             $headers .= "Bcc: akapinos@lapkinlab.ru\r\n";
+            ?>
+            <!-- Facebook Pixel Code -->
+            <script>
+                !function(f, b, e, v, n, t, s) {
+                    if (f.fbq) return;
+                    n = f.fbq = function() {
+                        n.callMethod ?
+                            n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+                    };
+                    if (!f._fbq) f._fbq = n;
+                    n.push    = n;
+                    n.loaded  = !0;
+                    n.version = '2.0';
+                    n.queue   = [];
+                    t         = b.createElement(e);
+                    t.async   = !0;
+                    t.src     = v;
+                    s         = b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t, s);
+                }(window, document, 'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '754412144956224');
+                fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+                           src="https://www.facebook.com/tr?id=754412144956224&ev=PageView&noscript=1"
+                /></noscript>
+            <!-- End Facebook Pixel Code -->
+            <?
             mail($to, $subject, $message, $headers);
             header("Location: /#spasibo");
         }
@@ -240,5 +295,6 @@ if (isset($_POST)) {
 } else {
     header("Location: /#error");
 }
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_before.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
 ?>
