f6cc1829ed119aaf72c41fabf6bdeb0925303392
deadie <zaq12wsx>
2020-01-02 09:26:53 +0300

    [upd] страница "Контакты"

--- contacts/index.php
+++ contacts/index.php
@@ -1,103 +1,47 @@
-<?
+<?php
+
+use LapkinLab\Helper;
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("keywords", "контакты labkinlab, контактная информация");
 $APPLICATION->SetPageProperty("title", "Контакты маркетингового агентства LapkinLAb в Москве");
 $APPLICATION->SetPageProperty("description", "Маркетинговое агентство LapkinLAb располагается по адресу: г. Москва, 2-й Котляковский пер., д. 18 &#9742; 8(800) 301-00-19 &#9993; mail@lapkinlab.ru");
 $APPLICATION->SetTitle("Контакты маркетингового агентства LapkinLAb в Москве");
-?><main> <section class="two">
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="breadcrumbs">
-				<div class="container_v2">
-					<div class="breadcrumbs__items">
-						<div class="breadcrumbs__content">
-							<div class="breadcrumbs__wrap">
-								<div class="breadcrumbs__item">
- <a href="/" class="breadcrumbs__item-link is-home" rel="home" title="Главная">Главная</a>
-								</div>
-								<div class="breadcrumbs__item">
-									<div class="breadcrumbs__item-sep">
-										 |
-									</div>
-								</div>
-								<div class="breadcrumbs__item">
- <span class="breadcrumbs__item-target">Контакты</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+
+$APPLICATION->AddChainItem($APPLICATION->GetTitle(), $APPLICATION->GetCurDir());
+?>
+
+<section class="page-contacts">
+    <div class="container">
+	    <div class="row">
+
+		    <div class="col-md-7 col-sm-12">
+                <script async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac6d4f483f8fbaf970c92434e432b8e8ec5c4538ec998e13dc167818cf13dbafd&amp;amp;width=100%25&amp;amp;height=450&amp;amp;lang=ru_RU&amp;amp;scroll=true"></script>
+		    </div>
+
+		    <div class="col-md-5 col-sm-12">
+                <h1>Контакты LapkinLab </h1>
+                <p><?= SITE_CONFIG['address'] ?><br><?= SITE_CONFIG['metro'] ?></p>
+                <p>
+                    Телефон: <a href="tel:<?= Helper::parsePhone(SITE_CONFIG['phone'], 'full') ?>"><?= Helper::parsePhone(SITE_CONFIG['phone'], 'formatted') ?></a></p>
+                <p>
+                    Письмо директору: <a href="mailto:ya@lapkinlab.ru">ya@lapkinlab.ru</a><br>
+                    Письмо Аккаунт-Менеджеру: <a href="mailto:dsenatorova@lapkinlab.ru">dsenatorova@lapkinlab.ru</a><br>
+                    Письмо по вакансиям: <a href="mailto:hr@lapkinlab.ru">hr@lapkinlab.ru</a><br>
+                    Прочие письма: <a href="mailto:mail@lapkinlab.ru">mail@lapkinlab.ru</a>
+                </p>
+                <p>Мы в социальных сетях:</p>
+                <p>
+                    <a href="<?= SITE_CONFIG['social_telegram'] ?>" rel="nofollow" target="_blank" title="Telegram"><?= renderSprite('icon-circle-telegram') ?></a>
+                    <a href="<?= SITE_CONFIG['social_telegram_bot'] ?>" rel="nofollow" target="_blank" title="Telegram Bot"><?= renderSprite('icon-circle-telegram') ?></a>
+                    <a href="<?= SITE_CONFIG['social_instagram'] ?>" rel="nofollow" target="_blank" title="Instagram"><?= renderSprite('icon-circle-instagram') ?></a>
+                    <a href="<?= SITE_CONFIG['social_vk'] ?>" rel="nofollow" target="_blank" title="Вконтакте"><?= renderSprite('icon-circle-vk') ?></a>
+                    <a href="<?= SITE_CONFIG['social_facebook'] ?>" rel="nofollow" target="_blank" title="Facebook"><?= renderSprite('icon-circle-facebook') ?></a>
+                </p>
 			</div>
+
 		</div>
 	</div>
-</div>
- </section> <section class="tenthre">
-<div class="container">
-	<div class="row">
-		<div class="col-md-9 col-sm-12">
-			 <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Aa7a3c79287b1fc621074ecc2abfb7f6a5097df06d86a534b35155044ad14f33a&amp;source=constructor"
-                    width="100%" height="450" frameborder="0"></iframe>
-		</div>
-		<div class="col-md-3 col-sm-12">
-			<div class="feedback_contacts_list">
-				<div class="feedback_contacts_item">
-					<div class="text">
-						<h1 class="title_wh_left">
-						Контакты LapkinLab </h1>
-						<p class="vvv">
- <br>
-							 Москва, Ленинская Слобода, д. 26<br>
-							 5 мин от метро "Автозаводская"<br>
- <b style="font-size: 22px; "><a class="roistat-phone" href="tel:88003010019" style="padding:10px 0px;">8 (800) 301-00-19</a></b><br>
-							 Письмо директору: <a href="mailto:ya@lapkinlab.ru">ya@lapkinlab.ru</a><br>
-                                                         Письмо Аккаунт-Менеджеру: <a href="mailto:dsenatorova@lapkinlab.ru">dsenatorova@lapkinlab.ru</a><br>
-							 Письмо по вакансиям: <a href="mailto:hr@lapkinlab.ru">hr@lapkinlab.ru</a><br>
-							 Прочие письма: <a href="mailto:mail@lapkinlab.ru">mail@lapkinlab.ru</a><br>
- <br>
-							 Мы в социальных сетях:<br>
- <a href="https://tele.click/lapkinlab" target="_blank" rel="noreferrer noopener"><i class="icon icon-telegram"></i></a> <a href="https://tele.click/lapkinlab_test_bot" target="_blank" rel="noreferrer noopener"><i class="icon icon-telegram"></i></a> <a href="https://www.instagram.com/lapkin___lab/" target="_blank" rel="noreferrer noopener"><i class="icon icon-instagram"></i></a> <a href="https://vk.com/lapkinlab" target="_blank" rel="noreferrer noopener"><i class="icon icon-vkontakte"></i></a> <a href="https://www.facebook.com/lapppkin" target="_blank" rel="noreferrer noopener"><i class="icon icon-facebook-squared"></i></a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
- </section> <section class="tenthre">
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<p class="title">Остались вопросы? Напишите нам!</p>
-		</div>
-		<div class="col-md-12 fomrbut">
-			<form action="/mail_post.php" method="post">
-                <input type="hidden" name="formname" value="Контакты">
-				<div class="col-md-4">
- <input type="text" name="your-name" placeholder="Ваше имя" required="">
-				</div>
-				<div class="col-md-4">
- <input type="text" name="your-phone" placeholder="Телефон" required="">
-				</div>
-				<div class="col-md-4">
- <input type="text" name="your-email" placeholder="Почта" required="">
-				</div>
-				<p style="display:none;">
- <input type="text" name="name" value="">
-				</p>
-				<div class="col-md-12" style="padding: 20px 15px;">
- <textarea cols="30" rows="10" name="your-message" placeholder="Напишите ваш вопрос для наших специалистов" required=""></textarea>
-				</div>
-				<div class="col-md-12" style="text-align: left;">
-					 Нажимая на кнопку Отправить заявку, я даю согласие на обработку персональных данных
-				</div>
-				<div class="col-md-12" style="text-align: center;">
- <input type="submit" class="tm_pb_button" name="" value="Отправить заявку" style="max-width: 255px;">
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
  </section>
-</main>
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
