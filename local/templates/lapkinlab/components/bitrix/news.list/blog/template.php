043459906a89d58905834825c345857a3956fe7d
deadie <zaq12wsx>
2020-01-03 08:08:45 +0300

    [upd] страница "Блог" (Новости)

---
+++ local/templates/lapkinlab/components/bitrix/news.list/blog/template.php
@@ -0,0 +1,80 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
+    <?= $arResult["NAV_STRING"] ?>
+<? endif; ?>
+
+<div class="blog--list">
+    <? foreach ($arResult["ITEMS"] as $arItem): ?>
+        <article class="blog--list--item">
+            <div class="blog--list--item-content">
+                <?
+                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                ?>
+
+                <? if ($arParams["DISPLAY_PICTURE"] != "N"): ?>
+                    <? if (is_array($arItem["PREVIEW_PICTURE"])): ?>
+                        <figure class="blog--list--item-thumbnail">
+                            <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
+                                <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
+                                    <img class="img-fluid"
+                                        src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
+                                        width="<?= $arItem["PREVIEW_PICTURE"]["WIDTH"] ?>"
+                                        height="<?= $arItem["PREVIEW_PICTURE"]["HEIGHT"] ?>"
+                                        alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
+                                        title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
+                                    >
+                                </a>
+                            <? else: ?>
+                                <img class="img-fluid"
+                                    src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
+                                    width="<?= $arItem["PREVIEW_PICTURE"]["WIDTH"] ?>"
+                                    height="<?= $arItem["PREVIEW_PICTURE"]["HEIGHT"] ?>"
+                                    alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
+                                    title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
+                                >
+                            <? endif; ?>
+                        </figure>
+                    <? endif; ?>
+                <? endif; ?>
+
+                <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
+                    <h2 class="blog--list--item-title">
+        				<? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
+                            <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= $arItem["NAME"] ?></a>
+                        <? else: ?>
+                            <?= $arItem["NAME"] ?>
+                        <? endif; ?>
+                    </h2>
+                <? endif; ?>
+
+                <div class="blog--list--item-info">
+                    <div class="blog--list--item-date"><?= $arItem['TIMESTAMP_X'] ?></div>
+                    <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
+                        <a class="btn btn-sm" href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= GetMessage("CT_BNL_GOTO_DETAIL") ?></a>
+                    <? endif; ?>
+                </div>
+            </div>
+        </article>
+    <? endforeach; ?>
+</div>
+
+<? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+    <?= $arResult["NAV_STRING"] ?>
+<? endif; ?>
+
--- novosti/index.php
+++ novosti/index.php
@@ -1,133 +1,95 @@
-<?
+<?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetPageProperty("title", "Блог маркетингового агентства LapkinLAb: новости и статьи");
 $APPLICATION->SetPageProperty("description", "Ознакомиться с актуальными новостями и полезными статьями в сфере интернет-продвижения можно на сайте маркетингового агентства LapkinLAb");
 $APPLICATION->SetTitle("Блог маркетингового агентства LapkinLAb: новости и статьи");
-
-var_dump($_REQUEST);
-
 ?>
 
-    <style>
-        .news_layout_list figure {
-            height: unset !important;
-        }
+<section class="page page-blog">
+    <div class="container">
+        <div class="row">
+            <h1 class="col-12">Новости</h1>
 
-        .news_layout_list figure img {
-            max-height: 300px;
-        }
-        .menu_logo {
-            margin-top: 0;
-        }
-        .row ul li::before {
-            display: none !important;
-        }
-    </style>
-    <main>
-
-        <div class="breadcrumbs">
-            <div class="container_v2">
-                <div class="breadcrumbs__items">
-                    <div class="breadcrumbs__content">
-                        <div class="breadcrumbs__wrap">
-                            <div class="breadcrumbs__item">
-                                <a href="/" class="breadcrumbs__item-link is-home" rel="home"
-                                   title="Главная">Главная</a>
-                            </div>
-                            <div class="breadcrumbs__item">
-                                <div class="breadcrumbs__item-sep">
-                                    |
-                                </div>
-                            </div>
-                            <div class="breadcrumbs__item">
-                                <span class="breadcrumbs__item-target">Новости</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="clear"></div>
-            </div>
-        </div>
-
-        <section class="news_layout">
-            <div class="container_v2">
-                <h1> Новости </h1><Br>
-                <?
-                $APPLICATION->IncludeComponent(
-                    "bitrix:news.list",
-                    "flat1",
-                    array(
-                        "IBLOCK_TYPE"                     => "news",
-                        "IBLOCK_ID"                       => "1",
-                        "NEWS_COUNT"                      => "12",
-                        "COMPONENT_TEMPLATE"              => "flat1",
-                        "SORT_BY1"                        => "ACTIVE_FROM",
-                        "SORT_ORDER1"                     => "DESC",
-                        "SORT_BY2"                        => "SORT",
-                        "SORT_ORDER2"                     => "ASC",
-                        "FILTER_NAME"                     => "",
-                        "FIELD_CODE"                      => array(
-                            0 => "",
-                            1 => "",
-                        ),
-                        "PROPERTY_CODE"                   => array(
-                            0 => "",
-                            1 => "",
-                        ),
-                        "CHECK_DATES"                     => "Y",
-                        "DETAIL_URL"                      => "/novosti/#ELEMENT_CODE#.html",
-                        "AJAX_MODE"                       => "N",
-                        "AJAX_OPTION_JUMP"                => "N",
-                        "AJAX_OPTION_STYLE"               => "Y",
-                        "AJAX_OPTION_HISTORY"             => "N",
-                        "AJAX_OPTION_ADDITIONAL"          => "",
-                        "CACHE_TYPE"                      => "A",
-                        "CACHE_TIME"                      => "36000000",
-                        "CACHE_FILTER"                    => "N",
-                        "CACHE_GROUPS"                    => "N",
-                        "PREVIEW_TRUNCATE_LEN"            => "",
-                        "ACTIVE_DATE_FORMAT"              => "d.m.Y",
-                        "SET_TITLE"                       => "Y",
-                        "SET_BROWSER_TITLE"               => "Y",
-                        "SET_META_KEYWORDS"               => "Y",
-                        "SET_META_DESCRIPTION"            => "Y",
-                        "SET_LAST_MODIFIED"               => "N",
-                        "INCLUDE_IBLOCK_INTO_CHAIN"       => "Y",
-                        "ADD_SECTIONS_CHAIN"              => "Y",
-                        "HIDE_LINK_WHEN_NO_DETAIL"        => "N",
-                        "PARENT_SECTION"                  => "",
-                        "PARENT_SECTION_CODE"             => "",
-                        "INCLUDE_SUBSECTIONS"             => "Y",
-                        "STRICT_SECTION_CHECK"            => "N",
-                        "COMPOSITE_FRAME_MODE"            => "A",
-                        "COMPOSITE_FRAME_TYPE"            => "AUTO",
-                        "PAGER_TEMPLATE"                  => "grid",
-                        "DISPLAY_TOP_PAGER"               => "N",
-                        "DISPLAY_BOTTOM_PAGER"            => "Y",
-                        "PAGER_TITLE"                     => "Новости",
-                        "PAGER_SHOW_ALWAYS"               => "N",
-                        "PAGER_DESC_NUMBERING"            => "N",
-                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-                        "PAGER_SHOW_ALL"                  => "N",
-                        "PAGER_BASE_LINK_ENABLE"          => "N",
-                        "SET_STATUS_404"                  => "Y",
-                        "SHOW_404"                        => "N",
-                        "MESSAGE_404"                     => "",
-                        "TEMPLATE_THEME"                  => "blue",
-                        "DISPLAY_DATE"                    => "Y",
-                        "DISPLAY_NAME"                    => "Y",
-                        "DISPLAY_PICTURE"                 => "Y",
-                        "DISPLAY_PREVIEW_TEXT"            => "Y",
-                        "MEDIA_PROPERTY"                  => "",
-                        "SLIDER_PROPERTY"                 => "",
-                        "SEARCH_PAGE"                     => "/search/",
-                        "USE_RATING"                      => "N",
-                        "USE_SHARE"                       => "N",
+            <div class="col-12">
+            <?
+            $APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                "blog",
+                array(
+                    "IBLOCK_TYPE"                     => "news",
+                    "IBLOCK_ID"                       => 1,
+                    "NEWS_COUNT"                      => 12,
+                    "COMPONENT_TEMPLATE"              => "flat1",
+                    "SORT_BY1"                        => "ACTIVE_FROM",
+                    "SORT_ORDER1"                     => "DESC",
+                    "SORT_BY2"                        => "SORT",
+                    "SORT_ORDER2"                     => "ASC",
+                    "FILTER_NAME"                     => "",
+                    "FIELD_CODE"                      => array(
+                        0 => "",
+                        1 => "",
                     ),
-                    false
-                );
-                ?>
+                    "PROPERTY_CODE"                   => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "CHECK_DATES"                     => "Y",
+                    "DETAIL_URL"                      => "/novosti/#ELEMENT_CODE#.html",
+                    "AJAX_MODE"                       => "N",
+                    "AJAX_OPTION_JUMP"                => "N",
+                    "AJAX_OPTION_STYLE"               => "Y",
+                    "AJAX_OPTION_HISTORY"             => "N",
+                    "AJAX_OPTION_ADDITIONAL"          => "",
+                    "CACHE_TYPE"                      => "A",
+                    "CACHE_TIME"                      => "36000000",
+                    "CACHE_FILTER"                    => "N",
+                    "CACHE_GROUPS"                    => "N",
+                    "PREVIEW_TRUNCATE_LEN"            => "",
+                    "ACTIVE_DATE_FORMAT"              => "d.m.Y",
+                    "SET_TITLE"                       => "Y",
+                    "SET_BROWSER_TITLE"               => "Y",
+                    "SET_META_KEYWORDS"               => "Y",
+                    "SET_META_DESCRIPTION"            => "Y",
+                    "SET_LAST_MODIFIED"               => "N",
+                    "INCLUDE_IBLOCK_INTO_CHAIN"       => "Y",
+                    "ADD_SECTIONS_CHAIN"              => "Y",
+                    "HIDE_LINK_WHEN_NO_DETAIL"        => "N",
+                    "PARENT_SECTION"                  => "",
+                    "PARENT_SECTION_CODE"             => "",
+                    "INCLUDE_SUBSECTIONS"             => "Y",
+                    "STRICT_SECTION_CHECK"            => "N",
+                    "COMPOSITE_FRAME_MODE"            => "A",
+                    "COMPOSITE_FRAME_TYPE"            => "AUTO",
+                    "PAGER_TEMPLATE"                  => "grid",
+                    "DISPLAY_TOP_PAGER"               => "N",
+                    "DISPLAY_BOTTOM_PAGER"            => "Y",
+                    "PAGER_TITLE"                     => "Новости",
+                    "PAGER_SHOW_ALWAYS"               => "N",
+                    "PAGER_DESC_NUMBERING"            => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL"                  => "N",
+                    "PAGER_BASE_LINK_ENABLE"          => "N",
+                    "SET_STATUS_404"                  => "Y",
+                    "SHOW_404"                        => "N",
+                    "MESSAGE_404"                     => "",
+                    "TEMPLATE_THEME"                  => "",
+                    "DISPLAY_DATE"                    => "Y",
+                    "DISPLAY_NAME"                    => "Y",
+                    "DISPLAY_PICTURE"                 => "Y",
+                    "DISPLAY_PREVIEW_TEXT"            => "Y",
+                    "MEDIA_PROPERTY"                  => "",
+                    "SLIDER_PROPERTY"                 => "",
+                    "SEARCH_PAGE"                     => "/search/",
+                    "USE_RATING"                      => "N",
+                    "USE_SHARE"                       => "N",
+                ),
+                false
+            );
+            ?>
             </div>
-        </section>
-    </main>
-<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+
+        </div>
+    </div>
+</section>
+
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
