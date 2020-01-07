0fa35ee2960ea52286840a2872829551b19264c5
deadie <zaq12wsx>
2020-01-07 15:49:32 +0300

    [fix] блок "Кейсы"

--- local/templates/lapkinlab/include_areas/section_cases.php
+++ local/templates/lapkinlab/include_areas/section_cases.php
@@ -1,90 +1,94 @@
 <!--наши кейсы-->
 <section class="section--cases">
     <div class="container">
-        <div class="section--cases--wrapper row">
-            <h2 class="section--cases--title col-12 text-center">Наши кейсы</h2>
-            <div class="section--cases--content col-xl-8 offset-xl-2 col-lg-12 offset-lg-0">
+        <div class="row">
+            <div class="col-12">
+                <div class="section--cases--wrapper row">
+                    <h2 class="section--cases--title col-12 text-center">Наши кейсы</h2>
+                    <div class="section--cases--content col-xl-8 offset-xl-2 col-lg-12 offset-lg-0">
 
-                <div class="row">
+                        <div class="row">
 
-                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" id="pills-fab-tab" data-toggle="pill" href="#pills-fab" role="tab" aria-controls="pills-fab" aria-selected="true">Фабрика штор на ткацкой</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="pills-medic-tab" data-toggle="pill" href="#pills-medic" role="tab" aria-controls="pills-medic" aria-selected="false">Медицинская компания</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="pills-broker-tab" data-toggle="pill" href="#pills-broker" role="tab" aria-controls="pills-broker" aria-selected="false">Кредитный брокер</a>
-                        </li>
-                    </ul>
+                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="pills-fab-tab" data-toggle="pill" href="#pills-fab" role="tab" aria-controls="pills-fab" aria-selected="true">Фабрика штор на ткацкой</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-medic-tab" data-toggle="pill" href="#pills-medic" role="tab" aria-controls="pills-medic" aria-selected="false">Медицинская компания</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-broker-tab" data-toggle="pill" href="#pills-broker" role="tab" aria-controls="pills-broker" aria-selected="false">Кредитный брокер</a>
+                                </li>
+                            </ul>
 
-                    <div class="tab-content" id="pills-tabContent">
+                            <div class="tab-content" id="pills-tabContent">
 
-                        <div class="tab-pane fade show active" id="pills-fab" role="tabpanel" aria-labelledby="pills-fab-tab">
-                            <div class="tab-pane--wrapper">
-                                <h3>Интернет-магазин по продаже штор, карнизов и сопутствующих товаров</h3>
-                                <div class="tab-pane--task">
-                                    <div class="task--label">Задача</div>
-                                    <div class="task--content">Организация полного цикла продаж через интернет. Поставлена цель по ежемесячному объему продаж.</div>
+                                <div class="tab-pane fade show active" id="pills-fab" role="tabpanel" aria-labelledby="pills-fab-tab">
+                                    <div class="tab-pane--wrapper">
+                                        <h3>Интернет-магазин по продаже штор, карнизов и сопутствующих товаров</h3>
+                                        <div class="tab-pane--task">
+                                            <div class="task--label">Задача</div>
+                                            <div class="task--content">Организация полного цикла продаж через интернет. Поставлена цель по ежемесячному объему продаж.</div>
+                                        </div>
+                                        <div class="tab-pane--result">
+                                            <div class="result--label">Результат</div>
+                                            <div class="result--content">Цели достигнуты в полном объеме. Сайт постоянно обновляется. За счет контекстной рекламы достигнута посещаемость 20 000 уникальных посетителей в месяц. Более 85% фраз в ТОП10.</div>
+                                        </div>
+                                        <div class="tab-pane--images">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/shatura1.png" alt="">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/shatura2.png" alt="">
+                                        </div>
+                                        <div class="tab-pane--desc">Продвижение в поисковых системах Яндекс и Google</div>
+                                    </div>
                                 </div>
-                                <div class="tab-pane--result">
-                                    <div class="result--label">Результат</div>
-                                    <div class="result--content">Цели достигнуты в полном объеме. Сайт постоянно обновляется. За счет контекстной рекламы достигнута посещаемость 20 000 уникальных посетителей в месяц. Более 85% фраз в ТОП10.</div>
+
+                                <div class="tab-pane fade" id="pills-medic" role="tabpanel" aria-labelledby="pills-medic-tab">
+                                    <div class="tab-pane--wrapper">
+                                        <h3>Срок: Март 2017 - Декабрь 2018</h3>
+                                        <div class="tab-pane--task">
+                                            <div class="task--label">Задача</div>
+                                            <div class="task--content">Продвинуть сайт с нуля, добиться получения от 100 заявок на оформление медицинских книжек в месяц .</div>
+                                        </div>
+                                        <div class="tab-pane--result">
+                                            <div class="result--label">Результат</div>
+                                            <div class="result--content">98% фраз находятся в ТОП-10, в среднем 6-7 тыс. уникальных посетителей в месяц, 215 заявок через форму обратной связи, около 700 обращений по телефону.</div>
+                                        </div>
+                                        <div class="tab-pane--images">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/m1.jpg" alt="">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/m2.jpg" alt="">
+                                        </div>
+                                        <div class="tab-pane--desc">Продвижение в поисковых системах Яндекс и Google</div>
+                                    </div>
                                 </div>
-                                <div class="tab-pane--images">
-                                    <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/shatura1.png" alt="">
-                                    <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/shatura2.png" alt="">
+
+                                <div class="tab-pane fade" id="pills-broker" role="tabpanel" aria-labelledby="pills-broker-tab">
+                                    <div class="tab-pane--wrapper">
+                                        <h3>Срок: Сентябрь 2018 - Декабрь 2018</h3>
+                                        <div class="tab-pane--task">
+                                            <div class="task--label">Задача</div>
+                                            <div class="task--content">Продвинуть сайт с нуля до топовых позиций по региону.</div>
+                                        </div>
+                                        <div class="tab-pane--result">
+                                            <div class="result--label">Результат</div>
+                                            <div class="result--content">Посещаемость сайта выросла до 150 уникальных посетителей в месяц, более 50% фраз в ТОП10.</div>
+                                        </div>
+                                        <div class="tab-pane--images">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/t1.jpg" alt="">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/t2.jpg" alt="">
+                                        </div>
+                                        <div class="tab-pane--desc">Продвижение в поисковых системах Яндекс и Google</div>
+                                    </div>
                                 </div>
-                                <div class="tab-pane--desc">Продвижение в поисковых системах Яндекс и Google</div>
+
                             </div>
-                        </div>
 
-                        <div class="tab-pane fade" id="pills-medic" role="tabpanel" aria-labelledby="pills-medic-tab">
-                            <div class="tab-pane--wrapper">
-                                <h3>Срок: Март 2017 - Декабрь 2018</h3>
-                                <div class="tab-pane--task">
-                                    <div class="task--label">Задача</div>
-                                    <div class="task--content">Продвинуть сайт с нуля, добиться получения от 100 заявок на оформление медицинских книжек в месяц .</div>
-                                </div>
-                                <div class="tab-pane--result">
-                                    <div class="result--label">Результат</div>
-                                    <div class="result--content">98% фраз находятся в ТОП-10, в среднем 6-7 тыс. уникальных посетителей в месяц, 215 заявок через форму обратной связи, около 700 обращений по телефону.</div>
-                                </div>
-                                <div class="tab-pane--images">
-                                    <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/m1.jpg" alt="">
-                                    <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/m2.jpg" alt="">
-                                </div>
-                                <div class="tab-pane--desc">Продвижение в поисковых системах Яндекс и Google</div>
-                            </div>
-                        </div>
-
-                        <div class="tab-pane fade" id="pills-broker" role="tabpanel" aria-labelledby="pills-broker-tab">
-                            <div class="tab-pane--wrapper">
-                                <h3>Срок: Сентябрь 2018 - Декабрь 2018</h3>
-                                <div class="tab-pane--task">
-                                    <div class="task--label">Задача</div>
-                                    <div class="task--content">Продвинуть сайт с нуля до топовых позиций по региону.</div>
-                                </div>
-                                <div class="tab-pane--result">
-                                    <div class="result--label">Результат</div>
-                                    <div class="result--content">Посещаемость сайта выросла до 150 уникальных посетителей в месяц, более 50% фраз в ТОП10.</div>
-                                </div>
-                                <div class="tab-pane--images">
-                                    <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/t1.jpg" alt="">
-                                    <img src="<?= SITE_TEMPLATE_PATH ?>/images/cases/t2.jpg" alt="">
-                                </div>
-                                <div class="tab-pane--desc">Продвижение в поисковых системах Яндекс и Google</div>
-                            </div>
                         </div>
 
                     </div>
-
+                    <div class="section--cases--order col-12 text-center">
+                        <a class="btn js-open-modal" href="#" data-action="openModal" data-modal="audit">Заказать услугу</a>
+                    </div>
                 </div>
-
-            </div>
-            <div class="section--cases--order col-12 text-center">
-                <a class="btn js-open-modal" href="#" data-action="openModal" data-modal="audit">Заказать услугу</a>
             </div>
         </div>
     </div>
