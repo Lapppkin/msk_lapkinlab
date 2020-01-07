e3704c7b0d67897d48e2b190936ca2c43772cf0b
deadie <zaq12wsx>
2020-01-07 11:01:23 +0300

    [fix] страница "Разработка сайтов"

--- local/templates/lapkinlab/include_areas/sites/sites_prices.php
+++ local/templates/lapkinlab/include_areas/sites/sites_prices.php
@@ -8,7 +8,7 @@
                     <h3 class="sites-price--item-title text-center h2">Mini-Landing page</h3>
                     <p class="sites-price--item-description text-center">Разработка мини промо-сайта. Подходит в первую очередь для проведения промо-акций, распродаж<br><a href="http://minilanding.lapkinlab.ru/" target="_blank">смотреть пример</a></p>
                     <div class="sites-price--item-prices">
-                        <?= renderSprite('rouble') ?> от 10 000 рублей
+                        <?= renderSprite('rouble') ?> от 10&nbsp;000&nbsp;&#8381;
                     </div>
                     <div class="sites-price--item-times">
                         <?= renderSprite('calendar') ?> от 7 до 10 дней
@@ -23,7 +23,7 @@
                     <h3 class="sites-price--item-title text-center h2">Landing page</h3>
                     <p class="sites-price--item-description text-center">Разработка мини промо-сайта. Включает в себя 5-7 экранов с информацией + форму обратной связи. </p>
                     <div class="sites-price--item-prices">
-                        <?= renderSprite('rouble') ?> от 40 000 рублей
+                        <?= renderSprite('rouble') ?> от 40&nbsp;000&nbsp;&#8381;
                     </div>
                     <div class="sites-price--item-times">
                         <?= renderSprite('calendar') ?> от 10 до 20 дней
@@ -38,7 +38,7 @@
                     <h3 class="sites-price--item-title text-center h2">Сайт услуг</h3>
                     <p class="sites-price--item-description text-center">Данный тип сайта служит для представления информации о деятельности компании, ее структуре и услугах</p>
                     <div class="sites-price--item-prices">
-                        <?= renderSprite('rouble') ?> от 50 000 рублей
+                        <?= renderSprite('rouble') ?> от 50&nbsp;000&nbsp;&#8381;
                     </div>
                     <div class="sites-price--item-times">
                         <?= renderSprite('calendar') ?> от 10 до 20 дней
@@ -53,7 +53,7 @@
                     <h3 class="sites-price--item-title text-center h2">Интернет-магазин</h3>
                     <p class="sites-price--item-description text-center">Разработка полнофункциональных интернет-магазинов, направленных на продажу как малого, так и большого количества товаров</p>
                     <div class="sites-price--item-prices">
-                        <?= renderSprite('rouble') ?> от 65 000 рублей
+                        <?= renderSprite('rouble') ?> от 65&nbsp;000&nbsp;&#8381;
                     </div>
                     <div class="sites-price--item-times">
                         <?= renderSprite('calendar') ?> от 30 до 45 дней
@@ -68,7 +68,7 @@
                     <h3 class="sites-price--item-title text-center h2">Корпоративный сайт</h3>
                     <p class="sites-price--item-description text-center">Призван предоставить пользователю всю информацию компании, ее товарах или услугах</p>
                     <div class="sites-price--item-prices">
-                        <?= renderSprite('rouble') ?> от 75 000 рублей
+                        <?= renderSprite('rouble') ?> от 75&nbsp;000&nbsp;&#8381;
                     </div>
                     <div class="sites-price--item-times">
                         <?= renderSprite('calendar') ?> от 25 до 40 дней
@@ -83,7 +83,7 @@
                     <h3 class="sites-price--item-title text-center h2">Дизайн/редизайн сайтов</h3>
                     <p class="sites-price--item-description text-center">Разработка современного дизайна (редизайна) сайта, который будет соответствовать трендам</p>
                     <div class="sites-price--item-prices">
-                        <?= renderSprite('rouble') ?> от 19 000 рублей
+                        <?= renderSprite('rouble') ?> от 19&nbsp;000&nbsp;&#8381;
                     </div>
                     <div class="sites-price--item-times">
                         <?= renderSprite('calendar') ?> от 10 до 20 дней
@@ -98,7 +98,7 @@
                     <h3 class="sites-price--item-title text-center h2">Разработка CRM-систем — автоматизация и полный контроль бизнеса</h3>
                     <p class="sites-price--item-description text-center">Мы занимаемся разработкой программ автоматизации отношений с вашими покупателями (CRM-системы), которые позволят держать под контролем все ключевые показатели бизнеса, настроить планирование и успешно наладить клиентоориентированность компании.<br><br>С помощью такой системы вы получите доступ к следующей информации:<br>• Портрет клиента<br>• Желания клиента<br>• История взаимодействия с покупателем «от и до»<br>• Аналитика работы с клиентом<br>• Точки входа и точки выхода клиента<br>• Контроль над менеджерами компании<br>• Слабые места маркетинговой стратегии</p>
                     <div class="sites-price--item-prices">
-                        <?= renderSprite('rouble') ?> от 15 000 рублей
+                        <?= renderSprite('rouble') ?> от 15&nbsp;000&nbsp;&#8381;
                     </div>
                     <div class="sites-price--item-times">
                         <?= renderSprite('calendar') ?> от 5 дней
