041b04ea8e36da08cad829cf4200bd18dae9f0c4
deadie <zaq12wsx>
2020-01-07 11:06:35 +0300

    [upd] обновлен блок "Наша активность"

--- local/templates/lapkinlab/include_areas/our_actions.php
+++ local/templates/lapkinlab/include_areas/our_actions.php
@@ -1,12 +1,20 @@
+<h4>Давайте зарабатывать вместе!</h4>
+<ul>
+    <li>Больше о блокчейне Minter вы можете узнать здесь <a target="_blank" href="https://minter.lapkinlab.ru/">https://minter.lapkinlab.ru/</a></li>
+    <li>О нашей монете можно почитать на странице <a target="_blank" href="https://minter.wiki/index.php?title=LapkinLab">https://minter.wiki/index.php?title=LapkinLab</a></li>
+</ul>
+<p>&nbsp;</p>
+
 <h4>Подписывайтесь на другие наши каналы:</h4>
 <ul>
     <li><a href="https://t-do.ru/SEO_lapkinlab" target="_blank">Продвижение бизнеса в интернете</a> — делюсь опытом как продвинуться в интернете, хитрости современного интернет маркетинга (<a href="/seo/">seo</a>, <a href="/ppc/">контекстная реклама</a>,
-        <a href="/smm/">smm</a>, <a href="/chat-bot/">telegram + боты</a> и каналы)</li>, <a href="/sites/">разработка сайтов</a>
+        <a href="/smm/">smm</a>, <a href="/chat-bot/">telegram + боты</a> и каналы), <a href="/sites/">разработка сайтов</a></li>
     <li><a href="https://t-do.ru/rutelegram_Lab" target="_blank">Обучение Telegram</a> — опыт создания/ведения Telegram каналов и пассивного заработка от их ведения.</li>
     <li><a href="http://rutelegramlab.lapkinlab.ru/" target="_blank">Курс по Telegram</a> — «Быстрый старт и первые деньги в Telegram с нуля» от&nbsp;<a href="https://t-do.ru/segmentkurs_bot">@RuTelegram_Lab</a></li>
     <li><a href="https://t-do.ru/gramtlgrm">TONGram Telegram</a> — канал посвящен новой криптовалюте «Gram» от Telegram и Павла Дурова основанной на блокчейн платформе Telegram Open Network (TON)</li>
 </ul>
 <p>&nbsp;</p>
+
 <h4>Также подписывайтесь на другие наши ресурсы:</h4>
 <ul>
     <li><a href="https://zen.yandex.ru/media/id/5ab942b5c3321b06f8b824a1">Блог на Яндекс.Дзен</a></li>
