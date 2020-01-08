c184770213acdad552f1b1cf93c179cabfbd0418
deadie <zaq12wsx>
2020-01-08 13:33:28 +0300

    [fix] ширина формы на страницах

--- local/templates/lapkinlab/include_areas/ppc/ppc_slider_form.php
+++ local/templates/lapkinlab/include_areas/ppc/ppc_slider_form.php
@@ -18,6 +18,6 @@ $params = array(
 );
 
 ?>
-<div class="slider--form col-xl-3 offset-xl-1 col-lg-5 offset-lg-0 col-md-12 offset-md-0">
+<div class="slider--form col-xl-4 offset-xl-0 col-lg-5 offset-lg-0 col-md-12 offset-md-0">
     <?php $APPLICATION->IncludeFile(SITE_TEMPLATE_PATH . '/include_areas/contact_form.php', $params) ?>
 </div>
