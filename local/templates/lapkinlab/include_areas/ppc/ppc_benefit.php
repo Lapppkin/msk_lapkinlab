c8cb7a243fa00866042c9ea8f5a167065efe6614
deadie <zaq12wsx>
2020-01-08 22:01:15 +0300

    [fix] исправления адаптива

--- local/templates/lapkinlab/include_areas/ppc/ppc_benefit.php
+++ local/templates/lapkinlab/include_areas/ppc/ppc_benefit.php
@@ -7,19 +7,19 @@
                 <div class="ppc-benefit--hole--wrapper">
                     <div class="hole-row">
                         <div class="hole-col hole-col-left">Бюджет на рекламу</div>
-                        <div class="hole-col hole-col-center">Клик 11,5 ₽<br>20 000 ₽<br>Посетителей <span>1739</span></div>
+                        <div class="hole-col hole-col-center old">Клик 11,5 ₽<br>20 000 ₽<br>Посетителей <span>1739</span></div>
                         <div class="hole-col hole-col-center">Клик 7,4 ₽<br>20 000 ₽<br>Посетителей <span>2667</span></div>
                         <div class="hole-col hole-col-right">&nbsp;</div>
                     </div>
                     <div class="hole-row">
                         <div class="hole-col hole-col-left">Конверсия сайта</div>
-                        <div class="hole-col hole-col-center">Продаж <span>104</span><br>6%</div>
+                        <div class="hole-col hole-col-center old">Продаж <span>104</span><br>6%</div>
                         <div class="hole-col hole-col-center">Продаж <span>159</span><br>6%</div>
                         <div class="hole-col hole-col-right">&nbsp;</div>
                     </div>
                     <div class="hole-row">
                         <div class="hole-col hole-col-left"> Средний чек 5 000 руб.</div>
-                        <div class="hole-col hole-col-center">Выручка<br><span>520 000 ₽</span></div>
+                        <div class="hole-col hole-col-center old">Выручка<br><span>520 000 ₽</span></div>
                         <div class="hole-col hole-col-center">Выручка<br><span>795 000 ₽</span></div>
                         <div class="hole-col hole-col-right">Выгода: 275 000 руб.</div>
                     </div>
