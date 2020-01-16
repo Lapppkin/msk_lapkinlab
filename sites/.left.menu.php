9006ac8f413eae6f2455f2c6df71524cce0c0e3a
deadie <zaq12wsx>
2020-01-16 14:44:33 +0300

    [del] страницы "расчета стоимости"

--- sites/.left.menu.php
+++ sites/.left.menu.php
@@ -1,46 +1,18 @@
-<?//
-//$aMenuLinks = Array(
-//	Array(
-//		"Портфолио",
-//		"/sites#portfolio",
-//		Array(),
-//		Array(),
-//		""
+<?php
+$aMenuLinks = array(
+    array(
+        'Калькулятор стоимости сайта',
+        '/sites/calculator/',
+        array(),
+        array(),
+        ''
+    ),
+//	array(
+//		'Портфолио',
+//		'/sites/portfolio/',
+//		array(),
+//		array(),
+//		''
 //	),
-//	Array(
-//		"Расчет стоимости создания лендинга",
-//		"/sites/raschet-stoimosti-lendinga/",
-//		Array(),
-//		Array(),
-//		""
-//	),
-//	Array(
-//		"Расчет стоимости создания мини лендинга",
-//		"/sites/raschet-stoimosti-minilendinga/",
-//		Array(),
-//		Array(),
-//		""
-//	),
-//	Array(
-//		"Расчет стоимости создания сайта интернет-магазина",
-//		"/sites/raschet-stoimosti-internet-magazina/",
-//		Array(),
-//		Array(),
-//		""
-//	),
-//	Array(
-//		"Расчет стоимости создания бизнес-сайта",
-//		"/sites/raschet-biznes-sajta/",
-//		Array(),
-//		Array(),
-//		""
-//	),
-//	Array(
-//		"Расчет стоимости создания корпоративного сайта",
-//		"/sites/raschet-korporativnogo-sajta/",
-//		Array(),
-//		Array(),
-//		""
-//	)
-//);
-//?>
+);
+
