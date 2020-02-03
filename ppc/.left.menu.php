38007c87f2fadf6db5ccfa2191291b75c9390057
deadie <zaq12wsx>
2020-02-03 15:29:52 +0300

    [add] страница Комплексное продвижение сайта (Контекстная реклама)

--- ppc/.left.menu.php
+++ ppc/.left.menu.php
@@ -1,11 +1,17 @@
 <?php
 $aMenuLinks = array(
     array(
-        'Контекстная реклама в Яндекс Директ',
+        'Контекстная реклама в Яндекс.Директ',
         '/ppc/yandex-direct/',
         array(),
         array(),
         ''
     ),
+    array(
+        'Комплексное продвижение сайта',
+        '/ppc/complex/',
+        array(),
+        array(),
+        ''
+    ),
 );
-
